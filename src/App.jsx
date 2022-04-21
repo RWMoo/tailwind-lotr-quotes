4ad74308b01a10a9c4670e90fe66bfa585976cf7
Rob Moore <83732271+RWMoo@users.noreply.github.com>
2022-04-21 17:59:11 +0930

    added smoke background / refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,48 @@
 import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
-import { wrap } from "popmotion";
 import "./styles/global.css";
-import { Example } from "./components/Example";
-import { Refresh } from "./components/Refresh";
+import smokeBackground from "./images/smoke3.png";
 import QuoteCard from "./components/QuoteCard";
+import { motion } from "framer-motion";
+
+const backgroundVariants = {
+  initial: {
+    scale: 1.5,
+  },
+  animate: {
+    scale: 2.5,
+    rotate: 45,
+    transition: {
+      duration: 30,
+    },
+    repeat: Infinity,
+    repeatMode: "mirror",
+  },
+};
 
 const App = () => {
-  const [count, setCount] = useState(0);
   return (
-     <div className="flex justify-center items-center h-screen overflow-hidden bg-gradient-to-br from-zinc-800 to-zinc-900">
-        <QuoteCard/>
-     </div>
+    <div className="flex relative justify-center items-center h-screen overflow-hidden bg-gradient-to-br from-zinc-800 to-zinc-900 ">
+      <div className="flex flex-col h-full justify-center items-center gap-8 lg:gap-16 -mt-32 lg:-mt-24">
+        <div className="text-center">
+          <h1 className="font-ringbearer text-lg lg:text-2xl text-yellow-600">
+            Lord of the Rings
+          </h1>
+          <h2 className="font-cormorant-light text-5xl lg:text-7xl text-white opacity-80">
+            Movie Quotes
+          </h2>
+        </div>
+        <QuoteCard />
+      </div>
+      <motion.img
+        variants={backgroundVariants}
+        initial="initial"
+        animate="animate"
+        src={smokeBackground}
+        className={
+          "w-screen h-screen object-center object-cover mix-blend-soft-light bg-blue-200 absolute opacity-70 "
+        }
+      />
+    </div>
   );
 };
 
--- src/components/QuoteCard.jsx
+++ src/components/QuoteCard.jsx
@@ -1,12 +1,11 @@
-import { AnimatePresence, MotionConfig } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
 import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { wrap } from "popmotion";
 import quotes from "../data/quotes.json";
 import smoke from "../images/smoke2.png";
-import gandalf from "../images/Aragorn_3.webp";
 import useWidth from "../hooks/useWidth";
-
+import { CaretCircleLeft, CaretCircleRight } from "phosphor-react";
 const swipePower = (offset, velocity) => {
   return Math.abs(offset) * velocity;
 };
@@ -15,13 +14,13 @@ const containerVariants = (screenWidth) => ({
   enter: (direction) => {
     return {
       x: direction < 0 ? -Math.abs(screenWidth) : screenWidth,
-      opacity: 0.7,
+      opacity: 0,
       scale: 1.5,
       rotateZ: 20,
       rotateY: -25,
       rotateX: -25,
       transition: {
-        duration: 2,
+        duration: 1,
       },
     };
   },
@@ -34,20 +33,21 @@ const containerVariants = (screenWidth) => ({
     rotateY: 0,
     rotateX: 0,
     transition: {
-      duration: 2,
+      duration: 1.5,
+      delay: 1.2,
     },
   },
   exit: (direction) => {
     return {
       zIndex: 0,
       x: direction < 0 ? screenWidth : -Math.abs(screenWidth),
-      opacity: 0.7,
       scale: 1.5,
       rotateZ: -20,
       rotateY: 25,
       rotateX: 25,
       transition: {
-        duration: 2,
+        duration: 1.2,
+        delay: 0.2,
       },
     };
   },
@@ -64,7 +64,7 @@ const smokeVariants = {
     scale: 3.5,
     transition: {
       scale: {
-        duration: 20,
+        duration: 30,
       },
       duration: 40,
       repeat: Infinity,
@@ -74,8 +74,9 @@ const smokeVariants = {
 };
 
 const imageVariants = {
-  initial: { scale: 1.1 },
+  initial: { scale: 1.1, opacity: 0.9 },
   animate: {
+    opacity: 0.8,
     scale: 1.4,
     transition: {
       duration: 30,
@@ -83,10 +84,29 @@ const imageVariants = {
   },
 };
 
+const buttonVariants = {
+  initial: {
+    opacity: 0,
+    scale: 1.2,
+  },
+  animate: {
+    opacity: 0.8,
+    scale: 1,
+    transition: {
+      delay: 2.5,
+    },
+  },
+  exit: {
+    scale: 1.2,
+    opacity: 0,
+  },
+};
+
 const QuoteCard = () => {
-  const width = useWidth();
+  const width = useWidth() + 200;
   const swipeConfidenceThreshold = 40000;
   const constraintsRef = useRef(null);
+  const [disabled, setDisabled] = useState(false);
   const [[page, direction], setPage] = useState([0, 0]);
   const quoteIndex = wrap(0, quotes.length, page);
 
@@ -103,47 +123,54 @@ const QuoteCard = () => {
     }
   };
 
-  const transition = {
-    opacity: { duration: 0.2 },
-  };
-
   const dragTransition = { bounceStiffness: 220, bounceDamping: 20 };
 
   return (
-    <motion.div
-      className="flex justify-center h-96 w-full"
-      ref={constraintsRef}
-    >
-      <AnimatePresence exitBeforeEnter custom={direction}>
-        <motion.div
-          key={page}
-          custom={direction}
-          variants={containerVariants(width)}
-          initial="enter"
-          animate="center"
-          exit="exit"
-          transition={transition}
-          drag
-          dragSnapToOrigin
-          dragTransition={dragTransition}
-          dragElastic={0.2}
-          dragMomentum={false}
-          onDragEnd={onDragEnd}
-          className="w-80 h-96 relative top-0 left-0 bg-gray-700 rounded-sm shadow-xl cursor-pointer overflow-hidden"
-        >
-          <Overlay />
-          <SmokeLayer />
-          <ImageLayer src={quotes[quoteIndex].image} />
-          <TextLayer quote={quotes[quoteIndex]} />
-        </motion.div>
-      </AnimatePresence>
-    </motion.div>
-  );
-};
-
-const Overlay = () => {
-  return (
-    <div className="relative top-0 left-0 w-full h-full bg-gradient-to-br to-gray-900 from-gray-500 opacity-40 z-30" />
+    <div className="flex justify-between items-center gap-8 h-1/2 z-40">
+      <motion.div className="h-full" ref={constraintsRef}>
+        <AnimatePresence className={"w-full"} custom={direction}>
+          <NextButton
+            key={`${page}next`}
+            paginate={paginate}
+            disabled={disabled}
+          />
+          <motion.div
+            key={page}
+            custom={direction}
+            variants={containerVariants(width)}
+            initial="enter"
+            animate={"center"}
+            exit={"exit"}
+            onAnimationStart={(definition) => {
+              if (definition === "exit") {
+                setDisabled(true);
+              }
+            }}
+            onAnimationComplete={(definition) => {
+              if (definition === "center") {
+                setDisabled(false);
+              }
+            }}
+            drag
+            dragSnapToOrigin
+            dragTransition={dragTransition}
+            dragElastic={0.2}
+            dragMomentum={false}
+            onDragEnd={onDragEnd}
+            className="w-64 md:w-96 h-full relative top-0 left-0 bg-gray-700 rounded-sm shadow-xl cursor-pointer overflow-hidden"
+          >
+            <SmokeLayer />
+            <ImageLayer src={quotes[quoteIndex].image} />
+            <TextLayer quote={quotes[quoteIndex]} />
+          </motion.div>
+          <PrevButton
+            key={`${page}prev`}
+            paginate={paginate}
+            disabled={disabled}
+          />
+        </AnimatePresence>
+      </motion.div>
+    </div>
   );
 };
 
@@ -157,7 +184,7 @@ const SmokeLayer = () => {
       variants={smokeVariants}
       initial={"initial"}
       animate={"animate"}
-      className="w-full absolute z-30 top-0 left-0 object-bottom  object-cover h-full"
+      className="w-full absolute z-30 top-0 left-0 object-bottom object-cover h-full"
       src={smoke}
     />
   );
@@ -173,7 +200,7 @@ const ImageLayer = ({ src }) => {
       variants={imageVariants}
       initial="initial"
       animate="animate"
-      className="w-full absolute top-0 left-0 object-top object-cover h-full "
+      className="w-full absolute top-0 left-0 object-top object-cover h-full"
       src={src}
     />
   );
@@ -182,16 +209,66 @@ const ImageLayer = ({ src }) => {
 const TextLayer = ({ quote }) => {
   return (
     <div className="absolute text-white top-0 left-0 z-30 h-full">
-      <div className="flex flex-col justify-between h-full p-8">
+      <div className="flex flex-col justify-between h-full p-6 md:p-7">
         <div>
-          <p className="font-cormorant mb-2">{quote.movie}</p>
-          <p className="font-ringbearer text-3xl">{quote.char}</p>
+          <p className="font-cormorant mb-1 text-sm md:text-lg">
+            {quote.movie}
+          </p>
+          <p
+            className={`font-ringbearer ${
+              quote.char.length > 12 ? "text-xl" : "text-3xl"
+            } md:text-3xl`}
+          >
+            {quote.char}
+          </p>
         </div>
-
-        <p className="font-cormorant-light text-xl leading-snug">{quote.dialog}</p>
+        <p
+          className={`font-cormorant-light ${
+            quote.dialog.length > 150 ? "text-base" : "text-xl"
+          }  md:text-xl leading-snug`}
+        >
+          {quote.dialog}
+        </p>
       </div>
     </div>
   );
 };
 
+const NextButton = ({ paginate, disabled }) => {
+  return (
+    <motion.button
+      disabled={disabled}
+      className="text-xl xs:text-2xl md:text-3xl lg:text-4xl text-white left-5 xs:left-4 sm:left-20 lg:left-40 xl:left-80 2xl:left-96 z-40 inset-y-0 absolute"
+      variants={buttonVariants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      onKeyPress={(e) => {
+        console.log(123);
+      }}
+      onClick={() => {
+        paginate(1);
+      }}
+    >
+      <CaretCircleLeft />
+    </motion.button>
+  );
+};
+
+const PrevButton = ({ paginate, disabled }) => {
+  return (
+    <motion.button
+      className="text-xl xs:text-2xl md:text-3xl lg:text-4xl text-white right-5 xs:right-4 sm:right-20 lg:right-40 xl:right-80 2xl:right-96 z-40 inset-y-0 absolute"
+      variants={buttonVariants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      onClick={() => {
+        paginate(-1);
+      }}
+    >
+      <CaretCircleRight />
+    </motion.button>
+  );
+};
 export default QuoteCard;
